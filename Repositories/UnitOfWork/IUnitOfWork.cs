438592578c0caec89743f3d3ae8e1e3913b16bb2
M N Tushar <mntushar25@gmail.com>
2022-07-15 10:28:05 +0600

    create unit of work

---
+++ Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Repositories.IRepositories;
+
+namespace Repositories.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IDepartmentRepositore Department { get; }
+
+        int Commit();
+        Task<int> CommitAsync();
+    }
+}
---
+++ Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using Repositories.AppContext;
+using Repositories.IRepositories;
+using Repositories.Repositrories;
+
+namespace Repositories.UnitOfWork
+{
+    public class UnitOfWork : IDisposable, IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        private IDepartmentRepositore _department { get; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IDepartmentRepositore Department => _ = _department ?? new DepartmentRepositore(_context);
+
+        public int Commit()
+        {
+            try
+            {
+                return _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        private bool disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+                if (disposing)
+                    _context.Dispose();
+
+            disposed = true;
+        }
+    }
+}
